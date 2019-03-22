daebbf99abd6d880c82687b34dace612642f8911
Sergey Zapuhlyak <zapuhlyak@gmail.com>
2019-03-22 17:28:42 +0300

    Added SectionTableViewCell.

---
+++ TelegramCharts/Sources/Cells/Section/SectionTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  SectionTableViewCell.swift
+//  TelegramCharts
+//
+//  Created by Sergey Zapuhlyak on 22/03/2019.
+//  Copyright © 2019 @zsergey. All rights reserved.
+//
+
+import UIKit
+
+class SectionTableViewCell: UITableViewCell {
+    
+}
---
+++ TelegramCharts/Sources/Cells/Section/SectionTableViewCellModel.swift
@@ -0,0 +1,20 @@
+//
+//  SectionTableViewCellModel.swift
+//  TelegramCharts
+//
+//  Created by Sergey Zapuhlyak on 22/03/2019.
+//  Copyright © 2019 @zsergey. All rights reserved.
+//
+
+import Foundation
+
+struct SectionTableViewCellModel {
+    var titleText: String
+}
+
+extension SectionTableViewCellModel: CellViewModelType {
+    
+    func setup(on cell: ButtonTableViewCell) {
+        
+    }
+}
